3549716426ee5eedae45cdefa126a910612e7671
Wacken <sebastianwalchi@gmail.com>
2020-07-04 21:08:42 +0200

    Training

---
+++ Katas/Medizine Clash/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Medizine_Clash
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ Katas/Train Reservation/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Train_Reservation
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
